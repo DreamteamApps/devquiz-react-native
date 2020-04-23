a869a4231914fd4771c161ee70875f34fecf20b4
Erick Alves <erick@erickalves.com.br>
2020-04-23 20:58:55 +0100

    addint theme provider to head

--- mobile/src/Components/Header/index.js
+++ mobile/src/Components/Header/index.js
@@ -2,26 +2,32 @@ import React from 'react';
 import {TouchableOpacity} from 'react-native';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import IconFeather from 'react-native-vector-icons/Feather';
+import {useTheme} from 'styled-components';
 
 import {Container, ButtonArea} from './styles';
 import {useNavigation} from '@react-navigation/native';
 
 export default function Header({close, back}) {
   const navigation = useNavigation();
+  const theme = useTheme();
 
   return (
     <Container>
       {close && (
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <ButtonArea>
-            <IconFeather name="x" size={30} color="#fff" />
+            <IconFeather name="x" size={30} color={theme.colors.secondary} />
           </ButtonArea>
         </TouchableOpacity>
       )}
       {back && (
         <TouchableOpacity onPress={() => navigation.goBack()}>
           <ButtonArea>
-            <IconIonicons name="ios-arrow-back" size={30} color="#fff" />
+            <IconIonicons
+              name="ios-arrow-back"
+              size={30}
+              color={theme.colors.secondary}
+            />
           </ButtonArea>
         </TouchableOpacity>
       )}
