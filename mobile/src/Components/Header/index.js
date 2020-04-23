8114b7730ce9743ec60bbbc475c60ee0c5d99a2b
Erick Alves <erick@erickalves.com.br>
2020-04-23 17:51:04 +0100

    header buttons padding

--- mobile/src/Components/Header/index.js
+++ mobile/src/Components/Header/index.js
@@ -3,7 +3,7 @@ import {TouchableOpacity} from 'react-native';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import IconFeather from 'react-native-vector-icons/Feather';
 
-import {Container} from './styles';
+import {Container, ButtonArea} from './styles';
 import {useNavigation} from '@react-navigation/native';
 
 export default function Header({close, back}) {
@@ -13,12 +13,16 @@ export default function Header({close, back}) {
     <Container>
       {close && (
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <IconFeather name="x" size={30} color="#fff" />
+          <ButtonArea>
+            <IconFeather name="x" size={30} color="#fff" />
+          </ButtonArea>
         </TouchableOpacity>
       )}
       {back && (
         <TouchableOpacity onPress={() => navigation.goBack()}>
-          <IconIonicons name="ios-arrow-back" size={30} color="#fff" />
+          <ButtonArea>
+            <IconIonicons name="ios-arrow-back" size={30} color="#fff" />
+          </ButtonArea>
         </TouchableOpacity>
       )}
     </Container>
--- mobile/src/Components/Header/styles.js
+++ mobile/src/Components/Header/styles.js
@@ -3,6 +3,9 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex-direction: row;
   justify-content: space-between;
-  padding: 25px;
+  padding: 15px;
   width: 100%;
+`;
+export const ButtonArea = styled.View`
+  padding: 10px;
 `;
