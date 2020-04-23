d350bbfb9f8d52e5b3f643e31d785828cf38a834
Erick Alves <erick@erickalves.com.br>
2020-04-23 01:36:16 +0100

    adding snackbar and changing structure to async

--- mobile/src/Screens/Main/index.js
+++ mobile/src/Screens/Main/index.js
@@ -1,22 +1,38 @@
-import React, {useState, useEffect} from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
 import {Container, Title, ButtonsContainer} from './styles';
 import {getData} from '~/Service/githubApi';
 import InputText from '../../Components/InputText';
 import CustomButton from '../../Components/CustomButton';
 import {saveUser} from '~/Utils/UserHelpers';
+import Snackbar from 'react-native-snackbar';
 
 export default function Main({navigation}) {
   const [username, setUsername] = useState('');
-  const getUserData = (username) => {
+
+  const getUserData = async (username) => {
     if (username) {
-      getData(username).then((dataReturn) => {
-        saveUser(dataReturn.data).then((_) => {
-          navigation.navigate('ModeSelect');
+      try {
+        const dataReturn = await getData(username);
+        const {login, avatar_url, name, public_repos} = dataReturn.data;
+        const user = {
+          login,
+          avatar: avatar_url,
+          name,
+          repos: public_repos,
+        };
+        await saveUser(user);
+        navigation.navigate('ModeSelect');
+      } catch (error) {
+        Snackbar.show({
+          text: 'Username not found. Check your username and try again',
+          duration: Snackbar.LENGTH_SHORT,
         });
-      });
+      }
     } else {
-      console.warn('insert your githut username');
+      Snackbar.show({
+        text: 'Type your github username',
+        duration: Snackbar.LENGTH_SHORT,
+      });
     }
   };
 
@@ -28,7 +44,6 @@ export default function Main({navigation}) {
         onChangeText={(text) => setUsername(text)}
         style={{width: 250}}
       />
-
       <ButtonsContainer>
         <CustomButton onPress={() => getUserData(username)}>Enter</CustomButton>
       </ButtonsContainer>
