a7a5328a204fc40a45de35fa005a843bc1e9a402
ricardovazcorrea <ricardovazcorrea@gmail.com>
2020-04-25 01:14:57 -0300

    Routes update, Create user, Create Match methods and CodeGenerator

--- backend/app/Controllers/Http/MatchController.js
+++ backend/app/Controllers/Http/MatchController.js
@@ -1,92 +1,54 @@
 'use strict'
 
+const User = use("App/Models/User")
+const Match = use("App/Models/Match")
+const generateCode = require("../../Helpers/CodeGenerator")
+
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
 /** @typedef {import('@adonisjs/framework/src/View')} View */
 
-/**
- * Resourceful controller for interacting with matches
- */
 class MatchController {
-  /**
-   * Show a list of all matches.
-   * GET matches
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async index ({ request, response, view }) {
-  }
 
   /**
-   * Render a form to be used for creating a new match.
-   * GET matches/create
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async create ({ request, response, view }) {
-  }
-
-  /**
-   * Create/save a new match.
+   * Create a new match for the user.
    * POST matches
    *
    * @param {object} ctx
    * @param {Request} ctx.request
    * @param {Response} ctx.response
    */
-  async store ({ request, response }) {
-  }
+  async store({ params, response }) {
+    try {
+      const userId = params.userid;
 
-  /**
-   * Display a single match.
-   * GET matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async show ({ params, request, response, view }) {
-  }
+      let existingUser = await User.findBy('id', userId);
+      if(!existingUser) {
+        return response.status(400).send({
+          "code": 1,
+          "message": "This user doesn't exists!"
+        });
+      }
 
-  /**
-   * Render a form to update an existing match.
-   * GET matches/:id/edit
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   * @param {View} ctx.view
-   */
-  async edit ({ params, request, response, view }) {
-  }
+      const code = generateCode();
 
-  /**
-   * Update match details.
-   * PUT or PATCH matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async update ({ params, request, response }) {
-  }
-
-  /**
-   * Delete a match with id.
-   * DELETE matches/:id
-   *
-   * @param {object} ctx
-   * @param {Request} ctx.request
-   * @param {Response} ctx.response
-   */
-  async destroy ({ params, request, response }) {
+      const createdMatch = await Match.create({
+        code: code,
+        owner_id: userId,
+        status: "pristine"
+      });
+      
+      return {
+        matchId: createdMatch.id,
+        matchCode: createdMatch.code 
+      };
+    } catch (error) {
+      return response.status(400).send({
+        "code": 2,
+        "message": "An error has occured!",
+        "error": error
+      });
+    }
   }
 }
 
