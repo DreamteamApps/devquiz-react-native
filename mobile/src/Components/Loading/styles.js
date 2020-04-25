d8920c6d6b659851ac5ffe847804ca922741d4c5
Erick Alves <erick@erickalves.com.br>
2020-04-25 22:48:15 +0100

    Loading component

---
+++ mobile/src/Components/Loading/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  justify-content: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+
+  position: absolute;
+  z-index: 10;
+`;
--- mobile/src/Contexts/AppContext.js
+++ mobile/src/Contexts/AppContext.js
@@ -5,16 +5,18 @@ import {ThemeProvider} from 'styled-components';
 import GameProvider from './GameContext';
 import AuthProvider from './AuthContext';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import Loading from '~/Components/Loading';
 
 export const AppContext = createContext();
 
 export default function AppProvider({children}) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   return (
     <AppContext.Provider value={{loading, setLoading}}>
       <ThemeProvider theme={styles}>
         <SafeAreaProvider>
           <AuthProvider>
+            {loading && <Loading />}
             <GameProvider>{children}</GameProvider>
           </AuthProvider>
         </SafeAreaProvider>
