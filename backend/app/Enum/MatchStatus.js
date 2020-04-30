2b759eb00ab0191bd9615fb671626b80689fcf4e
Erick Alves <erick@erickalves.com.br>
2020-04-30 19:10:38 +0100

    Cleaning repo

--- backend/app/Enum/MatchStatus.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    PRISTINE: 'pristine',
-    LOBBY: 'lobby',
-    PLAYING: 'playing',
-    DISCONNECTED: 'disconnected',
-    ENDED: 'ended'
-};
--- backend/app/Enum/UserType.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    USER: 'user',
-    ADMIN: 'admin'
-};
